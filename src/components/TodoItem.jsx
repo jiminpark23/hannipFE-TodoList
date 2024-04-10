f3e5ea2e7e161555921e2e1e78034280281cbee4
Jimin <jimin4124.22@gmail.com>
2024-04-11 00:39:13 +0900

    feat: todo update (check)

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./TodoItem.css";
 
-export default function TodoItem({ id, isDone, content, date }) {
+export default function TodoItem({ id, isDone, content, date, onUpdate }) {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
   return (
     <div className="TodoItem">
-      <input readOnly checked={isDone} type="checkbox" />
+      <input onChange={onChangeCheckbox} readOnly checked={isDone} type="checkbox" />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
