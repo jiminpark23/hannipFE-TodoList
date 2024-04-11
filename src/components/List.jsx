182bd7aeb5cf582defaaf383d1b1498b85546455
Jimin <jimin4124.22@gmail.com>
2024-04-11 23:51:23 +0900

    feat: useMemo

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 export default function List({ todos, onUpdate, onDelete }) {
   const [search, setSearch] = useState("");
@@ -21,9 +21,30 @@ export default function List({ todos, onUpdate, onDelete }) {
 
   const filteredTodos = getFilteredData();
 
+  // const getAnalyzedData = () => {
+  //   const totalCount = todos.length;
+  //   const doneCount = todos.filter((todo) => todo.isDone).length;
+  //   const notDoneCount = totalCount - doneCount;
+
+  //   return { totalCount, doneCount, notDoneCount };
+  // };
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return { totalCount, doneCount, notDoneCount };
+  }, [todos]);
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
   return (
     <div className="List">
       <h4>Todo List🌱</h4>
+      <div>total: {totalCount}</div>
+      <div>done: {doneCount}</div>
+      <div>notDone: {notDoneCount}</div>
       <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" />
       <div className="todos_wrapper">
         {filteredTodos.map((todo) => {
